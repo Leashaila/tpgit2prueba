a34181f3f2be44466e6b0d6576c971c8ff3a89d4
Leashaila <leandronmartinez17@gmail.com>
2020-04-08 13:50:04 -0300

    probando de nuevo

--- Calculadora.java
+++ Calculadora.java
@@ -12,7 +12,7 @@ public class Calculadora {
 		 do{
 		 System.out.println("Ingrese su operación /n 1 para sumar /n 2 para\r\n" + "restar /n 3 para multiplicar /n 4 para dividir");
 		 opcion = teclado.nextInt();
-		 }while(opcion < 1 && opcion > 4); 
+		 }while(opcion < 1 && opcion > 7); 
 		 System.out.println("Ingrese el primer numero");
 		 a = teclado.nextInt();
 		 System.out.println("Ingrese el segundo numero");
