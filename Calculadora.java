8f98ee809b1e51e85054a9f299d60de054cf63f1
Leashaila <leandronmartinez17@gmail.com>
2020-04-08 13:20:17 -0300

    probando de nuevo

---
+++ Calculadora.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		 Integer opcion;
+		 Integer a;
+		 Integer b;
+		 Integer resultado;
+		 System.out.println("holaaaaa");
+		 do{
+		 System.out.println("Ingrese su operación /n 1 para sumar /n 2 para\r\n" + "restar /n 3 para multiplicar /n 4 para dividir");
+		 opcion = teclado.nextInt();
+		 }while(opcion < 1 && opcion > 4); 
+		 System.out.println("Ingrese el primer numero");
+		 a = teclado.nextInt();
+		 System.out.println("Ingrese el segundo numero");
+		 b = teclado.nextInt();
+		 
+		 switch(opcion) {
+		 case 3:
+			 resultado = a+b;
+		 break;
+		 case 4:
+			 resultado = a-b;
+			 break;
+		 case 1:
+			 resultado = a*b;
+			 break;
+		 case 2:
+			 resultado = a/b;
+			 break;
+		 }
+		 }
+}
